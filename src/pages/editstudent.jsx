f8a724cd32c682b4bda22176312e0da71a25ef1a
godfreymaina <itsgoddy4pf@gmail.com>
2026-07-03 11:44:18 +0300

    diabled adm and input fields a good security feature

--- src/pages/editstudent.jsx
+++ src/pages/editstudent.jsx
@@ -101,6 +101,7 @@ const EditStudent = () => {
                     <label className="block mb-1 font-medium">Admission Number</label>
                     <input
                         type="number"
+                        disabled
                         className="w-full border-2 border-purple-700 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
                         value={formData.admNumber}
                         onChange={handleChange}
@@ -118,6 +119,8 @@ const EditStudent = () => {
                         onChange={handleChange}
                         name="email"
                         required
+                        disabled
+
                     />
                 </div>
 
